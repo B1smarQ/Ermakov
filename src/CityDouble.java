c12a1b7b9fab4a0b7fabc7b18681e48809635681
B1smarQ <bgh050021@gmail.com>
2024-10-19 22:10:50 +0400

    3.1 fix

--- src/CityDouble.java
+++ src/CityDouble.java
@@ -7,7 +7,8 @@ public class CityDouble extends City{
     protected void addWay(Way way) {
         if(ways.stream().anyMatch(x->x.getCityTo() == way.getCityTo())) throw new Stone("Way already exists");
         ways.add(way);
-        way.getCityTo().ways.add(new Way(this,way.getCost()));
+        if(way.getCityTo().ways.stream().noneMatch(x->x.getCityTo() == this))
+            way.getCityTo().ways.add(new Way(this,way.getCost()));
     }
 
     @Override
